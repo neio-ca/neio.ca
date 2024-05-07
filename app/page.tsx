604e4b78786a916cdf7c953d914fc4c1ee49c596
Jay (Huajie) Jin <jay.jin.henan@hotmail.com>
2024-05-07 18:30:29 -0400

    fix: Avoid horizontal scrollbar on small screen

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <p className="mt-8 mx-auto px-8 max-w-[600px]">
           We provide free software, software as a service (SaaS) products, web apps, mobile apps, and more, using the most advanced technologies including cloud and AI.
         </p>
-        <img src="images/home.jpg" alt="Tech 1" className="mt-8 mx-auto max-w-[600px]" />
+        <img src="images/home.jpg" alt="Tech 1" className="mt-8 w-full mx-auto max-w-[600px]" />
         {/* <h2 className="text-l font-semibold mt-4">Technologies We Use</h2>
         <div className="inline-flex items-center gap-2">
           <div>
