f33c9a16f1ee6adcac8af5449ead4f93e8d5d316
Jay (Huajie) Jin <jay.jin.henan@hotmail.com>
2024-05-07 17:37:18 -0400

    feat: Add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+	return (
+		<>
+			<p className="mt-16 mx-auto px-4 text-3xl font-semibold text-center"> Build software that people enjoy :) </p>
+			<p className="mt-16 mx-auto px-4 max-w-[600px]">NEIO Inc. is a small, independent company located in Ontario, Canada. We take pride in creating software that not only benefits people but also brings joy to our customers.</p>
+			<p className="mt-4 mx-auto px-4 max-w-[600px]">Unlike many businesses, we do not rely on outside investors, allowing us to follow our own vision and create products that resonate with our values. Our mission is to develop innovative software that adds value to your life while also supporting our own.</p>
+			<p className="mt-4 mx-auto px-4 max-w-[600px]">At NEIO Inc., we prioritize your privacy and strive to offer practical solutions that enhance your daily experiences. We hope that you find our products enjoyable and truly useful.</p>
+		</>
+	)
+}
--- components/footer/NEIOFooter.tsx
+++ components/footer/NEIOFooter.tsx
@@ -14,7 +14,7 @@ export function NEIOFooter() {
 							<h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h2>
 							<ul className="text-gray-500 dark:text-gray-400">
 								<li className="mb-4">
-									<a href="https://neio.ca/" className="hover:underline">About</a>
+									<a href="/about" className="hover:underline">About</a>
 								</li>
 								<li>
 									<a href="https://neio.ca/product/academy" className="hover:underline">NEIO Academy</a>
