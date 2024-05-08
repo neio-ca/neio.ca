f9c9d081143cfe2b241ae957f2cc09d9ca6a4e21
Jay (Huajie) Jin <jay.jin.henan@hotmail.com>
2024-05-07 21:32:35 -0400

    WIP: Create Privacy and Terms pages

---
+++ app/privacy/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Privacy() {
+	return (
+		<>
+			<p className="mt-16 mx-auto px-4 text-3xl font-semibold text-center">Privacy</p>
+			<p className="mt-16 mx-auto px-4 max-w-[600px]"></p>
+		</>
+	)
+}
